7f9548d544a729a16113c2373eb55f315aca75c3
Jia Zhang <jiaz@usafacts.org>
2022-06-10 13:33:19 -0400

    avg

--- colorScale.js
+++ colorScale.js
@@ -1,5 +1,25 @@
 	var colors = ["#CC6A19","#D99522","#E5BD3F","#F2E289","#F1F1F1","#97DFC7","#5CB79A","#00906E","#004837"]
+
+var colors = ["#3C2A9A",
+"#7756B2",
+"#A786CB",
+"#D3B9E6",
+"#F1F1F1",
+"#97DFC7",
+"#5CB79A",
+"#00906E",
+"#004837"]
+//
+
+var colors = ["#A786CB","#5CB79A","#5CB79A"]
+// var colors = ["#CC6A19",
+// "#E5BD3F",
+// "#F2E289",
+// "#97DFC7",
+// "#00906E",
+// "#004837"]
+var colors = ["#D99522","#D99522","#00906E"]
 	var difScale = difScale = d3.scaleThreshold()
-		//.domain([-30,-20,-10,-5,0,5,10,20,30])
 		.domain([-20,-15,-10,-5,0,5,10,15,20])
+		//.domain([-20,-10,-5,5,10,20])
 		.range(colors)
